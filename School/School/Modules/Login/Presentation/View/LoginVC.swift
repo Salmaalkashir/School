8d48e4de92f95529586e7f9b98e194f1de711fdd
Mahmoud810 <57998877+Mahmoud810@users.noreply.github.com>
2025-03-07 14:44:09 +0200

    change minimum deployment version

--- School/School/Modules/Login/Presentation/View/LoginVC.swift
+++ School/School/Modules/Login/Presentation/View/LoginVC.swift
@@ -24,7 +24,11 @@ class LoginVC: NibViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
     var loginAPI: LoginAPI = LoginAPI()
     @IBOutlet weak var chooseRoleLabel: UILabel!
+    
+    @IBOutlet weak var checkRemember: UIImageView!
     var viewModel: LoginViewModel
+    var selectedImage: Int = 0
+    var remember : Bool = false
     init(loginAPI: LoginAPI) {
         viewModel = LoginViewModel(api: loginAPI)
         super.init()
@@ -51,5 +55,52 @@ class LoginVC: NibViewController {
         passwordField.layer.cornerRadius = 12
         continueBtn.layer.cornerRadius = 12
     }
+    @IBAction func roleAction(sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            if selectedImage == 1{
+                // This block is executed when sender.tag is 0
+                parentImage.image = UIImage(named: "parent")!
+                teacherImage.image = UIImage(named: "selectedTeacher")!
+                selectedImage = 0 }
+            
+        case 1:
+            if selectedImage == 0{
+                // This block is executed when sender.tag is 1
+                teacherImage.image = UIImage(named: "teacher")!
+                parentImage.image = UIImage(named: "selectedParent")!
+                selectedImage = 1 }
+            
+        case 2:
+            remember.toggle()
+            checkRemember.image = UIImage(named: remember ? "checkbox" : "uncheck")
+            
+        default:
+            break
+        }
+    }
 
+}
+extension UITextField {
+    
+    func setLeftView (flagImage: UIImage, countycode: String) {
+        
+        let imageView = UIImageView(frame: CGRect(x: 8, y: 0, width: 32, height: 28))
+        imageView.image = flagImage
+        imageView.contentMode = .scaleAspectFit
+        
+        let codeLable = UILabel(frame: CGRect(x: 48, y: 0, width: 40, height: 28))
+        codeLable.text = countycode
+        codeLable.font = UIFont(name: "DMSans-Regular", size: 16)
+        codeLable.textColor = .black
+        
+        let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 90, height: 28))
+        leftView.addSubview(imageView)
+        leftView.addSubview(codeLable)
+        
+        self.leftView = leftView
+        self.leftViewMode = .always
+        
+    }
+    
 }
